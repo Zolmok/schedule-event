5089b89673f1b1ce0229a50f1a11adabb14ad649
Ricky Nelson <rickyn@socketwiz.com>
2020-02-01 21:14:54 -0500

    Updates

---
+++ precache-manifest.820bd333932f76f5d8147fa9b1c63dea.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04fbce63534fe49af77dac6bafd7574b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "81f08a9dd6da5b3157f4",
+    "url": "/static/css/2.c6231af7.chunk.css"
+  },
+  {
+    "revision": "76d66a65d05e5d8bc193",
+    "url": "/static/css/main.c5f4f6f0.chunk.css"
+  },
+  {
+    "revision": "81f08a9dd6da5b3157f4",
+    "url": "/static/js/2.629d5f43.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.629d5f43.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76d66a65d05e5d8bc193",
+    "url": "/static/js/main.7d1062a1.chunk.js"
+  },
+  {
+    "revision": "b52488f495d984f469c0",
+    "url": "/static/js/runtime-main.578d7822.js"
+  }
+]);
